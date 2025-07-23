e741b3b3e92c8b7630563e720c49410317a7242d
Trend25 <trendtakip2019@gmail.com>
2025-07-23 17:29:44 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-//import ClientAuthProvider from './ClientAuthProvider'  // Yeni dosya
-import { ClientAuthProvider } from './ClientAuthProvider';
+import ClientAuthProvider from './ClientAuthProvider';
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
