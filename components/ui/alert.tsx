9a28fb8fe98a3cb97a52f5344e19b4cc7afc73fb
Trend25 <trendtakip2019@gmail.com>
2025-07-25 15:08:13 +0300

    Update alert.tsx
    const Alert = React.forwardRef sonuna < eklendşi

--- components/ui/alert.tsx
+++ components/ui/alert.tsx
@@ -24,7 +24,7 @@ const alertVariants = cva(
   }
 )
 
-const Alert = React.forwardRef
+const Alert = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement> & VariantProps<typeof alertVariants>
 >(({ className, variant, ...props }, ref) => (
